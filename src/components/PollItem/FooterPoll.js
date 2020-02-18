afbf6df63534fc70de96d2ccbcc226f1f266cfc1
samslow <samuel.seo@lfin.kr>
2020-02-19 02:00:04 +0900

    Edit footerPoll color & apply neu morphism

--- src/components/PollItem/FooterPoll.js
+++ src/components/PollItem/FooterPoll.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
+import { NeomorphBox } from "react-native-neomorph-shadows";
+
+import COLOR from "theme/colorPallet";
 
 const FooterPoll = ({ scrollUp }) => {
+  const window = Dimensions.get("window");
   return (
     <TouchableOpacity
       onPress={() => scrollUp()}
@@ -10,11 +14,28 @@ const FooterPoll = ({ scrollUp }) => {
         alignItems: "center",
         justifyContent: "center",
         marginVertical: 20,
-        flexDirection: "row",
       }}
     >
-      <Feather name="arrow-up-circle" size={20} style={{ marginRight: 5 }} />
-      <Text>터치하여 위로 가기</Text>
+      <NeomorphBox
+        style={{
+          flexDirection: "row",
+          shadowRadius: 3,
+          borderRadius: 10,
+          backgroundColor: COLOR.bg,
+          width: window.width * 0.5,
+          height: 35,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Feather
+          name="arrow-up-circle"
+          size={20}
+          style={{ marginRight: 5 }}
+          color={COLOR.text}
+        />
+        <Text style={{ color: COLOR.text }}>터치하여 위로 가기</Text>
+      </NeomorphBox>
     </TouchableOpacity>
   );
 };
